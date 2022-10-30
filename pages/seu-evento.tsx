1d371597ffa19adfb4b82911de11ddf88cc9b3c8
pedroaurelli <pedroaureliano.contato@gmail.com>
2022-10-30 01:05:00 -0300

    Add router.isready in dependecy array

--- pages/seu-evento.tsx
+++ pages/seu-evento.tsx
@@ -7,16 +7,13 @@ export default function seuEvento () {
   const router = useRouter()
   const client = new SpotifyClient()
 
-  const accessToken = useAsync(() => client.getUserAccessToken(`${router.query.code}`), [ router.query ])
+  const accessToken = useAsync(() => client.getUserAccessToken(`${router.query.code}`), [ router.isReady ])
 
   const result = useAsync(() => client.getUserTopArtists(accessToken.result?.access_token || ''), [ accessToken.result ])
 
-  const currentUser = useAsync(() => client.getCurrentUserProfile(accessToken.result?.access_token || ''), [ accessToken.result?.access_token ])
-
   return (
     <>
-      <h1>Seu evento! {currentUser.result?.display_name}</h1>
-      <img src={currentUser.result?.images[0].url || ''} />
+      <h1>Seu evento!</h1>
       <ul>
         {result.result?.items.map((item, index) => {
           return (
