1489ddb8c53f51138a9b520c037075095356d532
pedroaurelli <pedroaureliano.contato@gmail.com>
2022-11-03 02:57:19 -0300

    Refact Footer to map itens

--- react/components/Footer/Footer.tsx
+++ react/components/Footer/Footer.tsx
@@ -1,7 +1,28 @@
 import { Box, IconButton, Stack, Typography } from '@mui/material'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import GitHubIcon from '@mui/icons-material/GitHub'
+import InstagramIcon from '@mui/icons-material/Instagram'
+
+type socialMediasRecordType = {
+  href: string
+  icon: ReactNode
+}
+
+const socialMediasRecord: socialMediasRecordType[] = [
+  {
+    href: 'https://www.instagram.com/pedroaurelli/',
+    icon: <InstagramIcon sx={{ height: '50px', width: '50px' }}/>
+  },
+  {
+    href: 'https://github.com/pedroaurelli',
+    icon: <GitHubIcon sx={{ height: '50px', width: '50px' }}/>
+  },
+  {
+    href: 'https://www.linkedin.com/in/pedro-aureliano/',
+    icon: <LinkedInIcon sx={{ height: '50px', width: '50px' }}/>
+  }
+]
 
 export default function Footer () {
   return (
@@ -18,20 +39,16 @@ export default function Footer () {
         justifyContent='center'
         spacing='8px'
       >
-        <IconButton
-          sx={{ height: '64px', width: '64px', color: '#B8B8B8' }}
-          href='https://github.com/pedroaurelli'
-          target='_blank'
-        >
-          <GitHubIcon sx={{ height: '50px', width: '50px' }}/>
-        </IconButton>
-        <IconButton
-          sx={{ height: '64px', width: '64px', color: '#B8B8B8' }}
-          href='https://www.linkedin.com/in/pedro-aureliano/'
-          target='_blank'
-        >
-          <LinkedInIcon sx={{ height: '50px', width: '50px' }}/>
-        </IconButton>
+        {socialMediasRecord.map((item, index) => (
+          <IconButton
+            key={index}
+            sx={{ height: '64px', width: '64px', color: '#B8B8B8' }}
+            href={item.href}
+            target='_blank'
+          >
+            {item.icon}
+          </IconButton>
+        ))}
       </Stack>
       <Typography fontWeight='300' color='#B8B8B8'>
         Development by Pedro Aureliano
